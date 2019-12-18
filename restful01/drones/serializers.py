d4a30b2fcfacd320025345b5615abd9cea7305bc
Carl Hinderer <carl.hinderer4@gmail.com>
2019-12-18 15:41:50 -0700

    Added serializer for Drones.

--- restful01/drones/serializers.py
+++ restful01/drones/serializers.py
@@ -20,3 +20,18 @@ class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
             'pk', 
             'name', 
             'drones')
+
+
+class DroneSerializer(serializers.HyperlinkedModelSerializer): 
+    # Display the category name 
+    drone_category = serializers.SlugRelatedField(queryset=DroneCategory.objects.all(), slug_field='name') 
+ 
+    class Meta: 
+        model = Drone 
+        fields = ( 
+            'url', 
+            'name', 
+            'drone_category', 
+            'manufacturing_date', 
+            'has_it_competed', 
+            'inserted_timestamp') 
